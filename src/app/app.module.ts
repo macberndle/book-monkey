56c0d3a5d8beee399aeae49d871aee3ae6d11241
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-04-22 10:29:11 +0200

    refactor(module): move components to books module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BookListComponent } from './book-list/book-list.component';
-import { BookListItemComponent } from './book-list-item/book-list-item.component';
-import { BookDetailsComponent } from './book-details/book-details.component';
+import { BookListComponent } from './books/book-list/book-list.component';
+import { BookListItemComponent } from './books/book-list-item/book-list-item.component';
+import { BookDetailsComponent } from './books/book-details/book-details.component';
 import { BooksModule } from './books/books.module';
 
 @NgModule({
