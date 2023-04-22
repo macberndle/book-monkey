8b1ac97b178a79631aa39047e2b2dff740bfaa77
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-04-22 10:33:06 +0200

    refactor(module): move declarations to books.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BookListComponent } from './books/book-list/book-list.component';
-import { BookListItemComponent } from './books/book-list-item/book-list-item.component';
-import { BookDetailsComponent } from './books/book-details/book-details.component';
 import { BooksModule } from './books/books.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BookListComponent,
-    BookListItemComponent,
-    BookDetailsComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, BooksModule],
   providers: [],
   bootstrap: [AppComponent],
