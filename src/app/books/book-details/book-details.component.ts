c19ce0396f5d2b918c5d908f0f0983f095eb6c57
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-05-06 09:51:38 +0200

    feat(use api): use api for detail component

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -2,7 +2,7 @@
 import { Component } from '@angular/core';
 import { Book } from '../../shared/book';
 import { BookStoreService } from 'src/app/shared/book-store.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'bm-book-details',
@@ -14,9 +14,20 @@ export class BookDetailsComponent {
 
   constructor(
     private service: BookStoreService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ){
     const isbn = this.route.snapshot.paramMap.get('isbn')!;
-    this.book = this.service.getSingle(isbn);
+    this.service.getSingle(isbn).subscribe(book => {
+      this.book = book;
+    });
+  }
+
+  removeBook(isbn: string): void {
+    if(window.confirm('Remove book?')){
+      this.service.remove(isbn).subscribe(() => {
+        this.router.navigateByUrl('/books');
+      })
+    }
   }
 }
