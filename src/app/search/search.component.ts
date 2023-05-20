f03f2f0f0ff6691f3a5e9db31679347068480919
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-05-20 08:59:24 +0200

    feat(typeahead): show search results

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { Subject, debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs';
+import { Observable, Subject, debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs';
 import { BookStoreService } from '../shared/book-store.service';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'bm-search',
@@ -9,15 +10,19 @@ import { BookStoreService } from '../shared/book-store.service';
 })
 export class SearchComponent {
   input$ = new Subject<string>();
+  isLoading = false;
+  results$: Observable<Book[]>
 
   constructor(
     private service: BookStoreService
   ){
-    this.input$.pipe(
+    this.results$ = this.input$.pipe(
         filter(term => term.length >= 3),
         debounceTime(500),
         distinctUntilChanged(),
-        switchMap(term => this.service.getAllSearch(term))
-      ).subscribe(e => console.log(e))
+        tap(() => this.isLoading = true),
+        switchMap(term => this.service.getAllSearch(term)),
+        tap(() => this.isLoading = false)
+      )
   }
 }
