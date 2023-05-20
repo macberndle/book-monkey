46506c0327b9b2973cd10103c0d4ab620f7ef6e4
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-05-20 08:43:49 +0200

    feat(typeahead): get books from service

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Subject, debounceTime, distinctUntilChanged, filter } from 'rxjs';
+import { Subject, debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'bm-search',
@@ -9,11 +10,14 @@ import { Subject, debounceTime, distinctUntilChanged, filter } from 'rxjs';
 export class SearchComponent {
   input$ = new Subject<string>();
 
-  constructor(){
+  constructor(
+    private service: BookStoreService
+  ){
     this.input$.pipe(
         filter(term => term.length >= 3),
         debounceTime(500),
-        distinctUntilChanged()
+        distinctUntilChanged(),
+        switchMap(term => this.service.getAllSearch(term))
       ).subscribe(e => console.log(e))
   }
 }
