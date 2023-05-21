12cdf111b583554c36d743c7e91c5bccd80f3c0a
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-05-21 10:55:37 +0200

    ref(interceptor): add login and logout button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './shared/auth.service';
 
 @Component({
   selector: 'bm-root',
@@ -6,4 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor(
+    public auth: AuthService
+  ) {}
 }
