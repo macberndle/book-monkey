0e6a8b505a01212e6bcc68ff059158e7e1f25b05
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-05-29 09:22:32 +0200

    feat(forms): add components
    add form component,
    add create component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { FormsModule } from '@angular/forms';
+import { BookFormComponent } from './book-form/book-form.component';
+import { BookCreateComponent } from './book-create/book-create.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BookFormComponent,
+    BookCreateComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule,
