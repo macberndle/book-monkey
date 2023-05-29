70a3270e819645c9740c006b1de70f0b0a93ac81
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-05-29 09:28:42 +0200

    feat(forms): add model to form comopnent

--- src/app/admin/book-form/book-form.component.ts
+++ src/app/admin/book-form/book-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from 'src/app/shared/book';
 
 @Component({
   selector: 'bm-book-form',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-form.component.css']
 })
 export class BookFormComponent {
-
+  book: Book = {
+    isbn: '',
+    title: '',
+    authors: ['']
+  };
 }
