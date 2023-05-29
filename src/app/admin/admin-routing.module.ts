20982ab5187f638212a1c6234d4bb09d4eb1a5b5
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-05-29 09:26:16 +0200

    feat(forms): add routing

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookCreateComponent } from './book-create/book-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    redirectTo: 'admin/create'
+  },
+  {
+    path: 'admin/create',
+    component: BookCreateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
