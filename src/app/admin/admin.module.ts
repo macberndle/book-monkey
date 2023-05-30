9b67012e0986fffdfdc03222fd20d580a2322a20
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-05-30 09:33:19 +0200

    refactor(reactive forms): add date values accessor and import reactive forms
    remove FormsModule,
    import reactive forms module,
    install date values accessor module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BookFormComponent } from './book-form/book-form.component';
 import { BookCreateComponent } from './book-create/book-create.component';
 
@@ -15,7 +15,7 @@ import { BookCreateComponent } from './book-create/book-create.component';
   imports: [
     CommonModule,
     AdminRoutingModule,
-    FormsModule
+    ReactiveFormsModule
   ]
 })
 export class AdminModule { }
