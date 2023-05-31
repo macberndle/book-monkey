df87b9a8d4b98be0b0b42039a0be92600f4e2cb3
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-05-31 08:52:15 +0200

    feat(edit book): add component
    add component to edit book,
    add button to call edit page

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,12 +5,14 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BookFormComponent } from './book-form/book-form.component';
 import { BookCreateComponent } from './book-create/book-create.component';
+import { BookEditComponent } from './book-edit/book-edit.component';
 
 
 @NgModule({
   declarations: [
     BookFormComponent,
-    BookCreateComponent
+    BookCreateComponent,
+    BookEditComponent
   ],
   imports: [
     CommonModule,
