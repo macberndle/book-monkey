a787dd48e0b220d5e654585ab3aa8a1457f4dd83
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-06-02 09:26:37 +0200

    feat(error messages): add component to show error messages

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,13 +6,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BookFormComponent } from './book-form/book-form.component';
 import { BookCreateComponent } from './book-create/book-create.component';
 import { BookEditComponent } from './book-edit/book-edit.component';
+import { FormErrorsComponent } from './form-errors/form-errors.component';
 
 
 @NgModule({
   declarations: [
     BookFormComponent,
     BookCreateComponent,
-    BookEditComponent
+    BookEditComponent,
+    FormErrorsComponent
   ],
   imports: [
     CommonModule,
