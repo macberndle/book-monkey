f96332fd5b05cae9e89663a3d8b3175be8115c44
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-06-09 10:30:04 +0200

    feat(loggedin only): add directive

---
+++ src/app/books/shared/loggedin-only.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, OnDestroy, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { AuthService } from 'src/app/shared/auth.service';
+
+@Directive({
+  selector: '[bmLoggedinOnly]'
+})
+export class LoggedinOnlyDirective implements OnDestroy {
+  private destroy$ = new Subject<void>();
+
+  constructor(
+    private authService: AuthService,
+    private template: TemplateRef<unknown>,
+    private viewContainer: ViewContainerRef
+  ) {
+    this.authService.isAuthenticated$.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(isAuthenticated => {
+      if(isAuthenticated){
+        this.viewContainer.createEmbeddedView(template);
+      } else {
+        this.viewContainer.clear();
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+      this.destroy$.next();
+  }
+}
