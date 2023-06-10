55cc31cd6bbe5b6849517eb98fb9ba8cf1088ddb
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-06-10 09:33:19 +0200

    feat(lazy loading): remove module import in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BooksModule } from './books/books.module';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { AuthInterceptor } from './shared/auth.interceptor';
-import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, SearchComponent],
   imports: [
     AppRoutingModule,
-    BooksModule,
     BrowserModule,
     HttpClientModule,
-    AdminModule,
   ],
   providers: [
     {
