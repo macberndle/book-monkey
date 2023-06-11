ddbd8e540651d698e8d020550b36b0c4cf7c5fa5
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-06-11 10:56:46 +0200

    refactor(standalone): make pipes and directives standalone

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -5,19 +5,22 @@ import { BooksRoutingModule } from './books-routing.module';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookListItemComponent } from './book-list-item/book-list-item.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
-import { IsbnPipe } from './shared/isbn.pipe';
-import { ConfirmDirective } from './shared/confirm.directive';
-import { LoggedinOnlyDirective } from './shared/loggedin-only.directive';
+import { IsbnPipe } from '../shared/isbn.pipe';
+import { ConfirmDirective } from '../shared/confirm.directive';
+import { LoggedinOnlyDirective } from '../shared/loggedin-only.directive';
 
 @NgModule({
   declarations: [
     BookListComponent,
     BookListItemComponent,
     BookDetailsComponent,
+  ],
+  imports: [
+    CommonModule,
+    BooksRoutingModule,
     IsbnPipe,
     ConfirmDirective,
     LoggedinOnlyDirective,
   ],
-  imports: [CommonModule, BooksRoutingModule],
 })
 export class BooksModule {}
--- src/app/shared/loggedin-only.directive.ts
+++ src/app/shared/loggedin-only.directive.ts
@@ -1,9 +1,10 @@
 import { Directive, OnDestroy, TemplateRef, ViewContainerRef } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
-import { AuthService } from 'src/app/shared/auth.service';
+import { AuthService } from './auth.service';
 
 @Directive({
-  selector: '[bmLoggedinOnly]'
+  selector: '[bmLoggedinOnly]',
+  standalone: true
 })
 export class LoggedinOnlyDirective implements OnDestroy {
   private destroy$ = new Subject<void>();
