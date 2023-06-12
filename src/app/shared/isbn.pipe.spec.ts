091697cc229d50696bd8980ae44b7f349b8ac2a8
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-06-12 09:19:05 +0200

    test(isbn pipe): add tests

--- src/app/shared/isbn.pipe.spec.ts
+++ src/app/shared/isbn.pipe.spec.ts
@@ -1,8 +1,21 @@
 import { IsbnPipe } from './isbn.pipe';
 
-describe('IsbnPipe', () => {
-  it('create an instance', () => {
-    const pipe = new IsbnPipe();
+describe('Pipe: IsbnPipe', () => {
+  let pipe: IsbnPipe;
+
+  beforeEach(() => {
+    pipe = new IsbnPipe();
+  })
+
+  it(`should create an instance`, () => {
     expect(pipe).toBeTruthy();
   });
+
+  it(`should format an ISBN with dash`, () => {
+    expect(pipe.transform('9783864909467')).toBe('978-3864909467')
+  });
+
+  it(`should ignore empty values`, () => {
+    expect(pipe.transform('')).toBe('')
+  })
 });
