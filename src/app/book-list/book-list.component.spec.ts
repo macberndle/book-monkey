c710c7847e12d96f05d77b4c74ec48bd7048e1a9
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-06-16 08:25:12 +0200

    test(bookmonkey): add tests for BookListComponent

--- src/app/book-list/book-list.component.spec.ts
+++ src/app/book-list/book-list.component.spec.ts
@@ -1,23 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Book } from '../shared/book';
 import { BookListComponent } from './book-list.component';
 
-describe('BookListComponent', () => {
+fdescribe('BookListComponent', () => {
   let component: BookListComponent;
-  let fixture: ComponentFixture<BookListComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BookListComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(BookListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new BookListComponent();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should hold a hardcoded list of 2 books`, () => {
+    expect(component.books).toHaveSize(2);
+  });
+
+  it(`should trigger an event on 'doSelect'`, () => {
+    const sentBook = {} as Book;
+    let receivedBook: Book | undefined;
+    component.selectBook.subscribe(book => {
+      receivedBook = book;
+    });
+
+    component.doSelect(sentBook);
+    expect(receivedBook).toBe(sentBook);
   });
 });
