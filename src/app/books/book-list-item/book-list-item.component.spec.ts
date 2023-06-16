f0068d0daef1b584f81409aebb441432bb0f2fe2
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-06-16 09:22:45 +0200

    test(bookmonkey): add test for BookListItemComponent

--- src/app/books/book-list-item/book-list-item.component.spec.ts
+++ src/app/books/book-list-item/book-list-item.component.spec.ts
@@ -1,22 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookListItemComponent } from './book-list-item.component';
+import { Book } from 'src/app/shared/book';
+import { Location } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('BookListItemComponent', () => {
+fdescribe('BookListItemComponent', () => {
   let component: BookListItemComponent;
   let fixture: ComponentFixture<BookListItemComponent>;
+  let location: Location;
+
+  const book: Book = {
+    isbn: '111',
+    title: 'Buch 1',
+    authors: [],
+    thumbnailUrl: 'https://cdn.ng-buch.de/kochen.png'
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [BookListItemComponent],
+      imports: [
+        RouterTestingModule.withRoutes([{ path: ':isbn', children: [] }]),
+      ],
     }).compileComponents();
 
+    location = TestBed.inject(Location);
     fixture = TestBed.createComponent(BookListItemComponent);
     component = fixture.componentInstance;
+    component.book = book;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should navigate to details page by ISBN`, async () => {
+    fixture.nativeElement.querySelector('img').click();
+    await fixture.whenStable();
+    expect(location.path()).toEqual('/111');
   });
 });
