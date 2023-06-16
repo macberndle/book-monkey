0571020819d8c1196a1c1626a5702effbbccbdde
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-06-16 08:32:13 +0200

    test(book monkey): add and modify tests for BookListComponent

--- src/app/book-list/book-list.component.spec.ts
+++ src/app/book-list/book-list.component.spec.ts
@@ -1,11 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Book } from '../shared/book';
 import { BookListComponent } from './book-list.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 fdescribe('BookListComponent', () => {
   let component: BookListComponent;
+  let fixture: ComponentFixture<BookListComponent>;
 
   beforeEach(async () => {
-    component = new BookListComponent();
+    await TestBed.configureTestingModule({
+      declarations: [BookListComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(BookListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
@@ -26,4 +36,14 @@ fdescribe('BookListComponent', () => {
     component.doSelect(sentBook);
     expect(receivedBook).toBe(sentBook);
   });
+
+  it(`should trigger an event on click`, () => {
+    let receivedBook: Book | undefined;
+    component.selectBook.subscribe(book => {
+      receivedBook = book;
+    });
+
+    fixture.nativeElement.querySelector('bm-book-list-item').click();
+    expect(receivedBook?.title).toBe('Tierisch gut kochen');
+  })
 });
