89404d18a0eaa518b8143be6a4c73cf18de95437
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-06-16 08:45:50 +0200

    test(bookmonkey): add tests for BookStoreService

--- src/app/shared/book-store.service.spec.ts
+++ src/app/shared/book-store.service.spec.ts
@@ -1,16 +1,39 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BookStoreService } from './book-store.service';
+import { Book } from './book';
+import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
-describe('BookStoreService', () => {
+fdescribe('BookStoreService', () => {
   let service: BookStoreService;
+  const expectedBooks: Book[] = [
+    { isbn: '111', title: 'Book 1', authors: [] },
+    { isbn: '222', title: 'Book 2', authors: [] }
+  ]
+  const httpStub = {
+    get: () => of(expectedBooks)
+  }
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpClient, useValue: httpStub }
+      ]
+    });
     service = TestBed.inject(BookStoreService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it(`should GET a list of all books`, () => {
+    let receivedBooks!: Book[];
+    service.getAll().subscribe(books => receivedBooks = books);
+
+    expect(receivedBooks).toHaveSize(2);
+    expect(receivedBooks[0].isbn).toBe('111');
+    expect(receivedBooks[1].isbn).toBe('222');
+  })
 });
