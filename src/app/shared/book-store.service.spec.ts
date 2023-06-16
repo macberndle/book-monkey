721f8e5e5a3a89317b3a58c1db7323fb0be50604
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-06-16 08:52:31 +0200

    test(bookmonkey): add and modify test for BookStoreService

--- src/app/shared/book-store.service.spec.ts
+++ src/app/shared/book-store.service.spec.ts
@@ -2,23 +2,31 @@ import { TestBed } from '@angular/core/testing';
 
 import { BookStoreService } from './book-store.service';
 import { Book } from './book';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 fdescribe('BookStoreService', () => {
   let service: BookStoreService;
+  let httpMock: {
+    get: (url: string) => Observable<Book[]>;
+  }
   const expectedBooks: Book[] = [
     { isbn: '111', title: 'Book 1', authors: [] },
     { isbn: '222', title: 'Book 2', authors: [] }
   ]
-  const httpStub = {
-    get: () => of(expectedBooks)
-  }
+  // const httpStub = {
+  //   get: () => of(expectedBooks)
+  // }
 
   beforeEach(() => {
+    httpMock = {
+      get: () => of(expectedBooks)
+    }
+    spyOn(httpMock, 'get').and.callThrough();
     TestBed.configureTestingModule({
       providers: [
-        { provide: HttpClient, useValue: httpStub }
+        { provide: HttpClient, useValue: httpMock },
+        BookStoreService
       ]
     });
     service = TestBed.inject(BookStoreService);
@@ -35,5 +43,7 @@ fdescribe('BookStoreService', () => {
     expect(receivedBooks).toHaveSize(2);
     expect(receivedBooks[0].isbn).toBe('111');
     expect(receivedBooks[1].isbn).toBe('222');
+
+    expect(httpMock.get).toHaveBeenCalledOnceWith('https://api5.angular-buch.com/books');
   })
 });
