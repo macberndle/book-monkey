65856058897608da2c56d16677849ebf0bb056ae
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-06-16 09:08:07 +0200

    test(bookmonkey): add and modify test for BookStoreService

--- src/app/shared/book-store.service.spec.ts
+++ src/app/shared/book-store.service.spec.ts
@@ -2,14 +2,15 @@ import { TestBed } from '@angular/core/testing';
 
 import { BookStoreService } from './book-store.service';
 import { Book } from './book';
-import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 fdescribe('BookStoreService', () => {
   let service: BookStoreService;
-  let httpMock: {
-    get: (url: string) => Observable<Book[]>;
-  }
+  let httpMock: HttpTestingController;
+  // let httpMock: {
+  //   get: (url: string) => Observable<Book[]>;
+  // }
   const expectedBooks: Book[] = [
     { isbn: '111', title: 'Book 1', authors: [] },
     { isbn: '222', title: 'Book 2', authors: [] }
@@ -19,18 +20,25 @@ fdescribe('BookStoreService', () => {
   // }
 
   beforeEach(() => {
-    httpMock = {
-      get: () => of(expectedBooks)
-    }
-    spyOn(httpMock, 'get').and.callThrough();
+    // httpMock = {
+    //   get: () => of(expectedBooks)
+    // }
+    // spyOn(httpMock, 'get').and.callThrough();
     TestBed.configureTestingModule({
-      providers: [
-        { provide: HttpClient, useValue: httpMock },
-        BookStoreService
-      ]
+      imports: [HttpClientTestingModule]
+      // providers: [
+      //   { provide: HttpClient, useValue: httpMock },
+      //   BookStoreService
+      // ]
     });
     service = TestBed.inject(BookStoreService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
+
+  afterEach(() => {
+    // prüfen, ob kein Request übrig geblieben ist
+    httpMock.verify();
+  })
 
   it('should be created', () => {
     expect(service).toBeTruthy();
@@ -40,10 +48,32 @@ fdescribe('BookStoreService', () => {
     let receivedBooks!: Book[];
     service.getAll().subscribe(books => receivedBooks = books);
 
+    // Request aus der Warteschlange holen
+    const req = httpMock.expectOne('https://api5.angular-buch.com/books');
+    expect(req.request.method).toEqual('GET');
+
+    // flush -- jetzt werden die Bücher emittiert
+    req.flush(expectedBooks);
+
     expect(receivedBooks).toHaveSize(2);
     expect(receivedBooks[0].isbn).toBe('111');
     expect(receivedBooks[1].isbn).toBe('222');
 
-    expect(httpMock.get).toHaveBeenCalledOnceWith('https://api5.angular-buch.com/books');
+    // expect(httpMock.get).toHaveBeenCalledOnceWith('https://api5.angular-buch.com/books');
+  });
+
+  it(`should throw a HttpErrorResponse on server error`, () => {
+    let errorResponse!: HttpErrorResponse;
+
+    service.getAll().subscribe({
+      error: (error: any) => errorResponse = error
+    });
+
+    const req = httpMock.expectOne('https://api5.angular-buch.com/books');
+
+    req.flush('Fehler!', {status: 500, statusText: 'Internal Server Error'});
+
+    expect(errorResponse.status).toBe(500);
+    expect(errorResponse.statusText).toBe('Internal Server Error');
   })
 });
