d558d1c585458827e484de2f95a6e7daad68aa1e
Bernd Ahnert <bernd.ahnert@gmail.com>
2023-06-29 08:00:39 +0200

    feat(book strore): add actions

--- src/app/books/store/book.actions.ts
+++ src/app/books/store/book.actions.ts
@@ -1,10 +1,25 @@
-import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
+import { Book } from 'src/app/shared/book';
 
-export const BookActions = createActionGroup({
-  source: 'Book',
-  events: {
-    'Load Books': emptyProps(),
-    'Load Books Success': props<{ data: unknown }>(),
-    'Load Books Failure': props<{ error: unknown }>(),
-  }
-});
+// export const BookActions = createActionGroup({
+//   source: 'Book',
+//   events: {
+//     '[Book] Load Books': emptyProps(),
+//     'Load Books Success': props<{ data: Book[] }>(),
+//     'Load Books Failure': props<{ error: string }>(),
+//   }
+// });
+
+export const loadBooks = createAction(
+  '[Book] Load Books'
+)
+
+export const loodBooksSuccess = createAction(
+  '[Book] Load Books Success',
+  props<{data: Book[]}>
+)
+
+export const loodBooksFailure = createAction(
+  '[Book] Load Books Failure',
+  props<{error: string}>
+)
